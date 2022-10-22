ae473748d71b8c5a511de98f0f268fa6d39492bb
pragnesh koli <pragnesh.softcolon@gmail.com>
2022-10-22 06:16:44 +0530

    done

--- app/src/main/java/com/example/softcolan_design/profile_tab3.java
+++ app/src/main/java/com/example/softcolan_design/profile_tab3.java
@@ -1,27 +1,23 @@
 package com.example.softcolan_design;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.fragment.app.Fragment;
+
 public class profile_tab3 extends Fragment {
     View view;
     ImageView img;
-    int man=5,female=6,ans;
-    int id;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view=inflater.inflate(R.layout.fragment_profile_tab3, container, false);
-        img=view.findViewById(R.id.image_man_female);
-//        man > female ? img.setImageResource(R.drawable.man) : img.setImageResource(R.drawable.female);
-        if(man>female)
+        view = inflater.inflate(R.layout.fragment_profile_tab3, container, false);
+        img = view.findViewById(R.id.image_man_female);
+        if (true)
         {
             img.setImageResource(R.drawable.man);
         }
@@ -29,6 +25,8 @@ public class profile_tab3 extends Fragment {
         {
             img.setImageResource(R.drawable.female);
         }
+
+
         return view;
     }
 }
